b2644467120d2b55f405ec65e78389fde74dcc64
Hatala Mark <hatala.mark@gmail.com>
2022-06-28 23:20:09 +0200

    home page modified

--- src/App.js
+++ src/App.js
@@ -6,28 +6,56 @@ import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import ForgotPassword from "./components/ForgotPassword";
 import ProtectedRoute from "./components/ProtectedRoute";
+import NewForm from "./components/NewForm";
+import Results from "./components/Results";
+import Members from "./components/Members"
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 function App() {
   return (
     <Container>
-          <UserAuthContextProvider>
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/" element={<Login />} />
+      <UserAuthContextProvider>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/new-form"
+            element={
+              <ProtectedRoute>
+                <NewForm />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/results"
+            element={
+              <ProtectedRoute>
+                <Results />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/members"
+            element={
+              <ProtectedRoute>
+                <Members />
+              </ProtectedRoute>
+            }
+          />
 
-              <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Login />} />
 
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-            </Routes>
-          </UserAuthContextProvider>
+          <Route path="/signup" element={<SignUp />} />
+
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </UserAuthContextProvider>
     </Container>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,15 @@ import { Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { Navigate } from "react-router-dom";
-import Logo from "../Logo/Nespresso-Logotype-Correct-2048x533-copy.png"
+import Logo from "../Logo/Nespresso-Logotype-Correct-2048x533-copy.png";
+import CheckLogo from "../Icons/Nespresso-Icons-05.png";
+import ResultsLogo from "../Icons/Nespresso-Icons-10.png";
+import MembersLogo from "../Icons/Nespresso-Icons-11.png";
+import '../css/home.css'
 
 export default function Home() {
   const [error, setError] = useState("");
   const { user, logOut } = useUserAuth();
-  
 
   async function handleLogOut() {
     setError("");
@@ -23,9 +26,10 @@ export default function Home() {
   }
 
   return (
-    <Navbar bg="light" fixed="top">
-      <Container>
-      <Navbar.Brand href="/home">
+    <>
+      <Navbar bg="light" fixed="top">
+        <Container>
+          <Navbar.Brand href="/home">
             <img
               src={Logo}
               width="160"
@@ -34,15 +38,45 @@ export default function Home() {
               alt="Nespresso logo"
             />
           </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text style={{paddingRight: "8px"}}>
-            Bejelentkezve mint: <a>{user && user.email}</a>
-          </Navbar.Text>
-          <Button variant="danger" onClick={handleLogOut}>
-            Kijelentkezés
-          </Button>
-        </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text style={{ paddingRight: "8px" }}>
+              Bejelentkezve mint: <a>{user && user.email}</a>
+            </Navbar.Text>
+            <Button variant="danger" onClick={handleLogOut}>
+              Kijelentkezés
+            </Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div className="col">
+            <a href="/new-form">
+              <div className="card shadow-sm">
+                <img src={CheckLogo} />
+                <p className="card-text">Új mérés</p>
+              </div>
+            </a>
+          </div>
+          <div className="col">
+            <a href="/results">
+              <div className="card shadow-sm">
+                <img src={ResultsLogo} />
+                <p className="card-text">Eredmények</p>
+              </div>
+            </a>
+          </div>
+          <div className="col">
+            <a href="/members">
+              <div className="card shadow-sm">
+                <img src={MembersLogo} />
+                <p className="card-text">Coffee Specialistek</p>
+              </div>
+            </a>
+          </div>
+        </div>
       </Container>
-    </Navbar>
+    </>
   );
 }
