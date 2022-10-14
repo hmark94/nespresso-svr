a1ccd32e158c0a1b5499cfa117fb0ade8913a243
Hatala <Mark.Hatala@nestle.com>
2022-10-14 15:20:37 +0200

    update

--- src/components/member results components/memberResults.component.jsx
+++ src/components/member results components/memberResults.component.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import ResultDataService from "../../context/ResultContext";
+import { Button, Card } from "react-bootstrap";
+import Chart from "chart.js/auto";
 
 import "./memberResults.styles.css";
 
 export default function MemberResults() {
   const [results, setResults] = useState([]);
+
   useEffect(() => {
     getResults();
   }, []);
 
   const getResults = async () => {
     const data = await ResultDataService.getAllResults();
-    console.log(data.docs);
+    /* console.log(data.docs); */
     setResults(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
 
+
   return (
     <>
-      <div>{JSON.stringify(results, undefined, 2)}</div>
+      <div className="results-body">
+        <div className="mb-4">
+          <a href="#">
+            <Card>
+              <Card.Header
+                style={{ background: "rgb(243 238 230)", fontWeight: "600" }}
+              >
+                {JSON.stringify(results, undefined, 2)}
+              </Card.Header>
+              <Card.Body>Body</Card.Body>
+            </Card>
+          </a>
+        </div>
+      </div>
     </>
   );
 }
