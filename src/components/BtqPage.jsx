b573f0c60856f9453269339e894ba24e1bdba58f
Hatala <Mark.Hatala@nestle.com>
2023-02-21 11:26:01 +0100

    query updated

--- src/components/BtqPage.jsx
+++ src/components/BtqPage.jsx
@@ -1,36 +1,51 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Spinner from './shared/Spinner'
-import BackButton from './shared/BackButton'
-import SVR_APP_DATA from '../context/DataBaseContext'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "./shared/Spinner";
+import BackButton from "./shared/BackButton";
+import SVR_APP_DATA from "../context/DataBaseContext";
+import { fdb } from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 function BtqPage() {
-  const [isLoading, setIsLoading] = useState(false)
-  const { route } = useParams()
-  const boutique = SVR_APP_DATA[0].items.find((item) => item.route === route)
+  const [isLoading, setIsLoading] = useState(false);
+  const { route } = useParams();
+  const boutique = SVR_APP_DATA[0].items.find((item) => item.route === route);
 
+  const resultsRef = collection(fdb, "surveyResponse");
 
+  const getResults = async () => {
+
+    const q = query(resultsRef, where("btq", "==", `${route}`));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+
+  };
+
+  useEffect(() => {
+    getResults();
+  }, [route]);
 
   return isLoading ? (
     <Spinner />
   ) : (
     <>
-      <section className='form-header mb-3 mt-6'>
+      <section className="form-header mb-3 mt-6">
         <BackButton />
 
-        <div className='d-flex align-items-center justify-content-between'>
-          <div className='m-4'>
+        <div className="d-flex align-items-center justify-content-between">
+          <div className="m-4">
             <h1>{boutique.boutique_name}</h1>
           </div>
         </div>
         <div></div>
       </section>
 
-      <div className='results-body'>
-        Eredmények
-      </div>
+      <div className="results-body">Eredmények</div>
     </>
-  )
+  );
 }
 
-export default BtqPage
+export default BtqPage;
--- src/components/member results components/memberResults.component.jsx
+++ src/components/member results components/memberResults.component.jsx
@@ -1,43 +1,60 @@
-import { useState, useEffect } from 'react'
-import { Spinner } from 'react-bootstrap'
-import './memberResults.styles.css'
-import BackButton from '../shared/BackButton'
-import { useParams } from 'react-router-dom'
-import { db } from '../../firebase'
-import { onValue, ref } from 'firebase/database'
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import "./memberResults.styles.css";
+import BackButton from "../shared/BackButton";
+import { useParams } from "react-router-dom";
+import { db, fdb } from "../../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { onValue, ref } from "firebase/database";
 
 export default function MemberResults() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [memberName, setMemberName] = useState('')
-  const { uuid } = useParams()
+  const [isLoading, setIsLoading] = useState(false);
+  const [memberName, setMemberName] = useState("");
+  const { uuid } = useParams();
 
-  const dataRef = ref(db, uuid)
+
+  const dataRef = ref(db, uuid);
+  const surveyResponseRef = collection(fdb, "surveyResponse");
 
   const getName = () => {
     onValue(dataRef, (snapshot) => {
-      const data = snapshot.val()
-      setMemberName(data.member)
-    })
-  }
+      const data = snapshot.val();
+      setMemberName(data.member);
+    });
+  };
+
+  const getResults = async () => {
+
+    const q = query(surveyResponseRef, where("uuid", "==", `${uuid}`));
+
+    const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data())
+      })
+  };
 
   useEffect(() => {
-    getName()
-  }, [memberName])
+    getResults();
+  }, [uuid]);
+
+  useEffect(() => {
+    getName();
+  }, [memberName]);
 
   return isLoading ? (
     <Spinner />
   ) : (
     <>
-      <section className='form-header mb-3 mt-6'>
+      <section className="form-header mb-3 mt-6">
         <BackButton />
 
-        <div className='d-flex align-items-center justify-content-between'>
-          <div className='m-4'>
+        <div className="d-flex align-items-center justify-content-between">
+          <div className="m-4">
             <h1>{memberName}</h1>
           </div>
         </div>
         <div></div>
       </section>
     </>
-  )
+  );
 }
