a9abc0824a6db22d2ac01dd8a1ec66eb9707c706
Hatala Mark <hatala.mark@gmail.com>
2023-03-02 22:02:17 +0100

    result page finished with chart

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -19,7 +19,7 @@ export default function Results() {
   const [average, setAverage] = useState(0)
   const [avPercentage, setAvPercentage] = useState(0)
   const [averageOfTheMonth, setAverageOfTheMonth] = useState(0)
-  const [btqChart, setBtqChart] = useState([])
+  const [chartData, setChartData] = useState([])
   const responseDocs = collection(fdb, 'surveyResponse')
 
   /*Date functions*/
@@ -117,6 +117,7 @@ export default function Results() {
       // push the average to monthlyAverages array
       monthlyAverages.push(average)
     }
+
     return monthlyAverages
   }
 
@@ -124,66 +125,88 @@ export default function Results() {
     calculateAverage()
   }, [])
 
-  const readData = SVR_APP_DATA[0].items.map((e) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const chartData = await Promise.all(
+        SVR_APP_DATA[0].items.map(async (e) => {
+          const monthlyAverages = await calculateMonthlyAverage(e.boutique_id);
+  
+          const data = {
+            labels: [
+              'Január',
+              'Február',
+              'Március',
+              'Április',
+              'Május',
+              'Június',
+              'Július',
+              'Augusztus',
+              'Szeptember',
+              'Október',
+              'November',
+              'December',
+            ],
+            datasets: [
+              {
+                label: 'Eredmények',
+                fill: false,
+                lineTension: 0.01,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 5,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: monthlyAverages,
+              },
+            ],
+          };
 
-    const data = {
-      labels: [
-        'Január',
-        'Február',
-        'Március',
-        'Április',
-        'Május',
-        'Június',
-        'Július',
-        'Augusztus',
-        'Szeptember',
-        'Október',
-        'November',
-        'December',
-      ],
-      datasets: [
-        {
-          label: 'Eredmények',
-          fill: false,
-          lineTension: 0.01,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: btqChart,
-        },
-      ],
-    }
-
-    return (
-      <div className='mb-4 w-auto' key={e.boutique_id}>
-        <Link to={`/results/${e.route}`}>
-          <Card className='card h-100'>
-            <Card.Header
-              className='card-header text-center'
-              style={{ background: 'rgb(243 238 230)', fontWeight: '600' }}
-            >
-              {e.boutique_name}
-            </Card.Header>
-            <Card.Body className='card-body d-flex justify-content-center'>
-              <Line data={data} />
-            </Card.Body>
-          </Card>
-        </Link>
-      </div>
-    )
-  })
+          const options = {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  min: 0,
+                  max: 100,
+                  stepSize: 10
+                }
+              }]
+            }
+          };
+  
+          return (
+            <div className='mb-4 w-auto' key={e.boutique_id}>
+              <Link to={`/results/${e.route}`}>
+                <Card className='card h-100'>
+                  <Card.Header
+                    className='card-header text-center'
+                    style={{ background: 'rgb(243 238 230)', fontWeight: '600' }}
+                  >
+                    {e.boutique_name}
+                  </Card.Header>
+                  <Card.Body className='card-body d-flex justify-content-center'>
+                    <Line data={data} options={options}/>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </div>
+          );
+        })
+      );
+      setChartData(chartData);
+    };
+  
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -206,7 +229,9 @@ export default function Results() {
         </h4>
       </div>
 
-      <div className='results-body mx-auto'>{readData}</div>
+      <div className='results-body mx-auto'>
+      {chartData}
+      </div>
     </>
   )
 }
